e86365ab19b9fb819bd30fbf91f0e12622e8814f
Felipe Marx <felipemxg@gmail.com>
2022-07-04 10:06:17 -0300

    Visualize Surface normals

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,28 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool HitSphere(const point3 center, double radius, const Ray r) {
+double HitSphere(const point3 center, double radius, const Ray r) {
     vec3 oc = r.origin - center;
     auto a = dot(r.direction, r.direction);
     auto b = 2.0 * dot(oc, r.direction);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 color RayColor(const Ray r) {
-    if (HitSphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t =  HitSphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = normalize(r.At(t) - vec3(0, 0, -1));
+        return 0.5 * color(N + 1);
     }
 
     vec3 unit_direction = normalize(r.direction);
-    auto t = 0.5 * (unit_direction.y) + 1.0;
+    t = 0.5 * ((unit_direction.y) + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
